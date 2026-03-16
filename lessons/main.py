066f834f18df5c07305290b98054286b6b962dd5
Kirill Kuzminykh <cykooz@gmail.com>
2026-03-16 20:42:16 +0300

    Added simple example of drawing

--- lessons/main.py
+++ lessons/main.py
@@ -10,6 +10,7 @@ def main():
     screen = pygame.display.set_mode(SIZE, pygame.SCALED, vsync=True)
     font = pygame.font.SysFont('Arial', 18, bold=False)
     pygame.display.set_caption('Pygame - Lesson')
+    screen.fill('black')
 
     clock = pygame.time.Clock()
     running = True
@@ -21,8 +22,9 @@ def main():
                 running = False
 
         # TODO: Write your code here ...
+        draw(screen)
 
-        fps_counter(screen, font, clock)
+        draw_fps_counter(screen, font, clock)
         # flip() the display to put your work on screen
         pygame.display.flip()
         clock.tick(60)  # limits FPS to 60
@@ -30,7 +32,23 @@ def main():
     pygame.quit()
 
 
-def fps_counter(screen, font, clock):
+def draw_fps_counter(screen, font, clock):
     fps = str(round(clock.get_fps(), 1))
     fps_t = font.render(fps, 1, pygame.Color('RED'), 'black')
     screen.blit(fps_t, (0, 0))
+
+
+def draw(screen: pygame.Surface):
+    screen.fill('black')
+    width, height = screen.get_size()
+
+    pygame.draw.rect(
+        screen,
+        'white',
+        (100, 100, width - 200, height - 200),
+    )
+
+    mid_x, mid_y = width // 2, height // 2
+    pygame.draw.line(screen, 'red', (100, mid_y), (width - 101, mid_y))
+
+    screen.set_at((mid_x, mid_y - 50), 'black')
